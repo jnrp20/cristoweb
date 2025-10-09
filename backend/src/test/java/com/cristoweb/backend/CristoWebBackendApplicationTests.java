6ff6d121f68bec91f41d7e4008e73ee839179f2e
jnrp20 <jromeropineda443@gmail.com>
2025-10-09 18:44:48 -0500

    Estructura inicial de CristoWeb (backend + frontend)

---
+++ backend/src/test/java/com/cristoweb/backend/CristoWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristoweb.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CristoWebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
